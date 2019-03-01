0ce30e87d1280b44aa61a9bfbeabe3a0d2d3e6a3
jlagr <j.gonzalez@digitalairstrike.com>
2019-03-01 16:58:34 -0600

    Preparing keys to deploy

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI_1: "mongodb://brad:brad@ds231725.mlab.com:31725/devconnector",
-  mongoURI:
-    "mongodb://jlagr:Ocampo1318@tutorialscluster-shard-00-00-3ujwc.mongodb.net:27017,tutorialscluster-shard-00-01-3ujwc.mongodb.net:27017,tutorialscluster-shard-00-02-3ujwc.mongodb.net:27017/test?ssl=true&replicaSet=TutorialsCluster-shard-0&authSource=admin&retryWrites=true",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
---
+++ config/keys_prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI_1: process_env.MONGO_URI,
+  secretOrKey: process_env.SECRET_OR_KEY
+};
