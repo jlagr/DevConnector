f8987f127618ab9f87e76a0d057288e42753a0da
jlagr <j.gonzalez@digitalairstrike.com>
2019-03-02 07:24:20 -0600

    Prepare for deploy 4

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,2 @@
-module.exports = {
-  mongoURI: process_env.MONGO_URI,
-  secretOrKey: process_env.SECRET_OR_KEY
-};
+export const mongoURI = process_env.MONGO_URI;
+export const secretOrKey = process_env.SECRET_OR_KEY;
